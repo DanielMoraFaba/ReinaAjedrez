8dc7e8d6b57f8e4bb9c0890887e49883dd2efbf4
Daniel <morafabadaniel@gmail.com>
2022-11-28 21:04:50 +0100

    Clase MainApp
    1. Crea el atributo de clase reina. Realiza un commit.
    2. Crea el método ejecutarOpcion que dependiendo de la opción pasada como parámetro, actuará en consecuencia. Realiza un commit.
    3. Crea el método crearReinaDefecto que asignará al atributo de clase reina una nueva instancia de una reina creada con el constructor por defecto. Realiza un commit.
    4. Crea el método crearReinaColor que asignará al atributo de clase reina una nueva instancia de una reina creada con el constructor al que le pasamos el color. Realiza un commit.
    5. Crea el método mover que mostrará un menú con las posibles direcciones, nos preguntará por la dirección y la cantidad de pasos a mover y moverá la reina según esos parámetros. Realiza un commit.
    6. Crea el método mostrarReina que nos mostrará la información de la reina si ésta está creada o de lo contrario nos informará de ello.
    7. Crea el método main que será el método principal de nuestra aplicación y deberá iterar mostrando el menú principal, pidendo la opción y ejecutándola mientras no elijamos salir, en cuyo caso mostrará un mensaje de despedida y nuestra aplicación finalizará. Realiza un commit y realiza el push a tu repositorio remoto en GitHub.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,85 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+import javax.naming.OperationNotSupportedException;
+import static org.iesalandalus.programacion.reinaajedrez.Consola.despedida;
+
 public class MainApp {
-	
+
 	public static void main(String[] args) {
-		
+
+		do {
+			Consola.mostrarMenu();
+			ejecutarOpcion(Consola.elegirOpcionMenu());
+		} while (!salir);
+
 	}
+
+	static Reina reina;
+	static boolean salir = false;
+
+	public static void ejecutarOpcion(int eleccion) {
+
+		switch (eleccion) {
+
+			case 1:
+				crearReinaDefecto();
+				break;
+
+			case 2:
+				crearReinaColor();
+				break;
+
+			case 3:
+				mover();
+				break;
+
+			case 4:
+				despedida();
+				salir = true;
+				break;
+		}
+
+	}
+
+	public static void crearReinaDefecto() {
+
+		try {
+			reina = new Reina();
+			mostrarReina();
+		} catch (IllegalArgumentException | NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public static void crearReinaColor() {
+
+		try {
+			reina = new Reina(Consola.elegirColor());
+			mostrarReina();
+		} catch (IllegalArgumentException | NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public static void mover() {
+
+		try {
+			Consola.mostrarMenuDirecciones();
+			reina.mover(Consola.elegirDireccion(), Consola.elegirPasos());
+			mostrarReina();
+		} catch (IllegalArgumentException | NullPointerException e) {
+			System.out.println(e.getMessage());
+		} catch (OperationNotSupportedException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public static void mostrarReina() {
+		System.out.println(reina.toString());
+	}
+
 }
